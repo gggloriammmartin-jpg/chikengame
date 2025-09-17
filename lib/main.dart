7356afbcb97ed026bade2fa3df9d07751fd8f804
gggloriammmartin-jpg <maksim.khlebnikov@example.com>
2025-09-17 19:03:42 +0600

    add keystore generation for release builds

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:chiken_odyssey/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+ 
 void main() async {
   runZonedGuarded(
     () async {
